cb3b78a187cba8d43003852805b7ad4e22272926
Pratap HC <pratapgowda355@gmail.com>
2023-06-15 11:12:41 +0530

    Initial Commit

---
+++ EmployeeCrudBlazer/Data/Employee.cs
@@ -0,0 +1,41 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace EmployeeCrudBlazer.Data
+{
+    public class Employee
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "First Name is required")]
+        public string FirstName { get; set; }
+
+        [Required(ErrorMessage = "Middle Name is required")]
+        public string MiddleName { get; set; }
+
+        [Required(ErrorMessage = "Last Name is required")]
+        public string LastName { get; set; }
+
+        public string FullName => $"{FirstName} {MiddleName} {LastName}";
+
+        [Required(ErrorMessage = "Postal Code is required")]
+        public string PostalCode { get; set; }
+
+        [Required(ErrorMessage = "Address is required")]
+        public string Address { get; set; }
+
+        [Required(ErrorMessage = "Email Address is required")]
+        [EmailAddress(ErrorMessage = "Invalid Email Address")]
+        public string EmailAddress { get; set; }
+
+        [Required(ErrorMessage = "Phone Number is required")]
+        [Phone(ErrorMessage = "Invalid Phone Number")]
+        public string PhoneNumber { get; set; }
+
+        [Required(ErrorMessage = "City is required")]
+        public string City { get; set; }
+
+        [Required(ErrorMessage = "Designation is required")]
+        public string Designation { get; set; }
+    }
+}
---
+++ EmployeeCrudBlazer/Data/EmployeeService.cs
@@ -0,0 +1,53 @@
+﻿using EmployeeCrudBlazer.Context;
+using Microsoft.EntityFrameworkCore;
+
+namespace EmployeeCrudBlazer.Data
+{
+    public class EmployeeService
+    {
+        private readonly ApplicationDbContext _applicationDbContext;
+        public EmployeeService(ApplicationDbContext applicationDbContext) 
+        {
+            _applicationDbContext = applicationDbContext;
+        }
+
+        //get all employee
+        public async Task<List<Employee>> GetEmployees()
+        {
+            return await _applicationDbContext.Employees.ToListAsync();
+        }
+        //add new employee
+        public async Task<bool> AddNewEmployee(Employee employee)
+        {
+
+             _applicationDbContext.Employees.AddAsync(employee);
+
+            await _applicationDbContext.SaveChangesAsync();
+            return true;
+
+        }
+        //get employee by ID
+        public async Task<Employee> GetEmployeeById(int id)
+        {
+           Employee employee = await _applicationDbContext.Employees.FirstOrDefaultAsync(x => x.Id == id);
+            return employee;
+
+        }
+        //update Employee by ID
+        public async Task<bool> UpdateEmployeeById(Employee employee)
+        {
+            _applicationDbContext.Employees.Update(employee);
+            await _applicationDbContext.SaveChangesAsync();
+            return true;
+
+        }
+        //delete emoloyee
+        public async Task<bool> DeleteEmployeeRecords(Employee employee)
+        {
+            _applicationDbContext.Employees.Remove(employee);
+            await _applicationDbContext.SaveChangesAsync();
+            return true;
+
+        }
+    }
+}
